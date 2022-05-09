01ea36f6ec708c09e9e90beb374ffcd3a8c1557b
palakdevgan <m7.palak@gmail.com>
2022-05-09 11:16:35 -0400

    added functions for Employees and Roles

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const inquirer =require('inquirer');
 const CompanyDb = require('./lib/CompanyDb');
-
+const db=new CompanyDb();
 
 const showMenu = () =>{
     return inquirer.prompt({
@@ -12,6 +12,14 @@ const showMenu = () =>{
 
 };
 
-showMenu().then(answers =>{
-    return CompanyDb.getDepartments();
-})
+showMenu().then(({action}) =>{
+    if(action === 'View all Departments'){
+    return db.getDepartments();
+    }
+    else if(action === 'View all Roles'){
+        return db.getRoles();
+        }
+        else if(action === 'View all Employees'){
+            return db.getEmployees();
+            }
+});
