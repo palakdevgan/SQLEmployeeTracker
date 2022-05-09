0eb09f7fbec89aaca145b5046411d3e2ca5786aa
palakdevgan <m7.palak@gmail.com>
2022-05-09 13:02:56 -0400

    added features to view and add departments

--- index.js
+++ index.js
@@ -12,14 +12,100 @@ const showMenu = () =>{
 
 };
 
+const addDepartment = () =>{
+  return inquirer.prompt({
+    type:'input',
+    name:'deptName',
+    message:"What is the Department name? (Required)",
+    validate:deptNameInput => {
+      if ((typeof deptNameInput === 'string') && deptNameInput && isNaN(deptNameInput)) 
+              {
+                return true;
+
+              } 
+              else 
+              {
+                console.log("\nPlease enter valid Department name!");
+                return false;
+              }
+    },
+    filter: deptNameInput => {
+      // clear the invalid input
+      return ((typeof deptNameInput === 'string') && deptNameInput && isNaN(deptNameInput)) ? deptNameInput : ''
+  }
+  })
+};
+
+const addRole = () =>{
+    return inquirer.prompt([
+    {
+      type:'input',
+      name:'roleTitle',
+      message:"What is the Role Title? (Required)",
+      validate:roleTitleInput => {
+        if ((typeof roleTitleInput === 'string') && roleTitleInput && isNaN(roleTitleInput)) 
+                {
+                  return true;
+  
+                } 
+                else 
+                {
+                  console.log("\nPlease enter valid Role Title!");
+                  return false;
+                }
+      },
+      filter: roleTitleInput => {
+        // clear the invalid input
+        return ((typeof roleTitleInput === 'string') && roleTitleInput && isNaN(roleTitleInput)) ? roleTitleInput : ''
+    }
+    }
+    ,
+    {
+        type:'input',
+        name:'roleSalary',
+        message:"What is the Role's Salary? (Required)",
+        validate: roleSalaryInput => {
+          if (isNaN(roleSalaryInput) || !roleSalaryInput) 
+          {
+            console.log("\nPlease enter valid Role Salary!");
+          } 
+          else 
+          {
+            return true;
+          }
+        },
+        filter: roleSalaryInput => {
+          // clear the invalid input
+          return !(isNaN(roleSalaryInput) || !roleSalaryInput) ? roleSalaryInput :''
+      }
+    },
+    {
+        type:'list',
+        message: 'What would you like to do?',
+        name:'action',
+        choices:db.getDepartments()
+    }
+])
+  };
+
 showMenu().then(({action}) =>{
     if(action === 'View all Departments'){
     return db.getDepartments();
     }
     else if(action === 'View all Roles'){
-        return db.getRoles();
+    return db.getRoles();
+    }
+    else if(action === 'View all Employees'){
+    return db.getEmployees();
+    }
+    else if(action === 'Add a Department'){
+    addDepartment().then(answers => {
+    return db.addDepartment(answers.deptName);
+    })
+    }
+    else if(action === 'Add a Role'){
+        addRole().then(answers => {
+       console.log(answers);
+        })
         }
-        else if(action === 'View all Employees'){
-            return db.getEmployees();
-            }
 });
