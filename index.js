edf0d4dc1e1303f83e2511d65235c59f7434a09d
palakdevgan <m7.palak@gmail.com>
2022-05-10 11:45:13 -0400

    added code to add dept

--- index.js
+++ index.js
@@ -1,113 +1,119 @@
-const inquirer =require('inquirer');
+const inquirer = require('inquirer');
 const CompanyDb = require('./lib/CompanyDb');
-const db=new CompanyDb();
+const db = new CompanyDb();
 
-
-const showMenu = () =>{
+const showMenu = () => {
     return inquirer.prompt({
-        type:'list',
+        type: 'list',
         message: 'What would you like to do?',
-        name:'action',
-        choices:['View all Departments','View all Roles','View all Employees','Add a Department','Add a Role','Add an Employee','Update an Employee Role']
+        name: 'action',
+        choices: ['View all Departments', 'View all Roles', 'View all Employees', 'Add a Department', 'Add a Role', 'Add an Employee', 'Update an Employee Role']
     })
 
 };
 
-const addDepartment = () =>{
-  return inquirer.prompt({
-    type:'input',
-    name:'deptName',
-    message:"What is the Department name? (Required)",
-    validate:deptNameInput => {
-      if ((typeof deptNameInput === 'string') && deptNameInput && isNaN(deptNameInput)) 
-              {
+const addDepartment = () => {
+    return inquirer.prompt({
+        type: 'input',
+        name: 'deptName',
+        message: "What is the Department name? (Required)",
+        validate: deptNameInput => {
+            if ((typeof deptNameInput === 'string') && deptNameInput && isNaN(deptNameInput)) {
                 return true;
 
-              } 
-              else 
-              {
+            }
+            else {
                 console.log("\nPlease enter valid Department name!");
                 return false;
-              }
-    },
-    filter: deptNameInput => {
-      // clear the invalid input
-      return ((typeof deptNameInput === 'string') && deptNameInput && isNaN(deptNameInput)) ? deptNameInput : ''
-  }
-  })
+            }
+        },
+        filter: deptNameInput => {
+            // clear the invalid input
+            return ((typeof deptNameInput === 'string') && deptNameInput && isNaN(deptNameInput)) ? deptNameInput : ''
+        }
+    })
 };
 
-const addRole = () =>{
-    var myArray = db.getDepartments();
-    return inquirer.prompt([
-    {
-      type:'input',
-      name:'roleTitle',
-      message:"What is the Role Title? (Required)",
-      validate:roleTitleInput => {
-        if ((typeof roleTitleInput === 'string') && roleTitleInput && isNaN(roleTitleInput)) 
-                {
-                  return true;
-  
-                } 
-                else 
-                {
-                  console.log("\nPlease enter valid Role Title!");
-                  return false;
-                }
-      },
-      filter: roleTitleInput => {
-        // clear the invalid input
-        return ((typeof roleTitleInput === 'string') && roleTitleInput && isNaN(roleTitleInput)) ? roleTitleInput : ''
-    }
-    }
-    ,
-    {
-        type:'input',
-        name:'roleSalary',
-        message:"What is the Role's Salary? (Required)",
-        validate: roleSalaryInput => {
-          if (isNaN(roleSalaryInput) || !roleSalaryInput) 
-          {
-            console.log("\nPlease enter valid Role Salary!");
-          } 
-          else 
-          {
-            return true;
-          }
-        },
-        filter: roleSalaryInput => {
-          // clear the invalid input
-          return !(isNaN(roleSalaryInput) || !roleSalaryInput) ? roleSalaryInput :''
-      }
-    },
-    {
-        type:'list',
-        message: 'What would you like to do?',
-        name:'action',
-        choices:myArray
-    }
-])
-  };
+const addRole = () => {
+    db.returnDepartments().then(([rows]) => {
+        let departments = rows;
+        const departmentChoices = departments.map(({ id, DeptName }) => ({
+            name: DeptName,
+            value: id
+        }))
+    //console.log(departmentChoices);
+    return    inquirer.prompt([
+            {
+                type: 'input',
+                name: 'roleTitle',
+                message: "What is the Role Title? (Required)",
+                validate: roleTitleInput => {
+                    if ((typeof roleTitleInput === 'string') && roleTitleInput && isNaN(roleTitleInput)) {
+                        return true;
 
-showMenu().then(({action}) =>{
-    if(action === 'View all Departments'){
-    console.log(db.getDepartments());
-    }
-    else if(action === 'View all Roles'){
-    return db.getRoles();
-    }
-    else if(action === 'View all Employees'){
-    return db.getEmployees();
-    }
-    else if(action === 'Add a Department'){
-    addDepartment().then(answers => {
-    return db.addDepartment(answers.deptName);
+                    }
+                    else {
+                        console.log("\nPlease enter valid Role Title!");
+                        return false;
+                    }
+                },
+                filter: roleTitleInput => {
+                    // clear the invalid input
+                    return ((typeof roleTitleInput === 'string') && roleTitleInput && isNaN(roleTitleInput)) ? roleTitleInput : ''
+                }
+            }
+            ,
+            {
+                type: 'input',
+                name: 'roleSalary',
+                message: "What is the Role's Salary? (Required)",
+                validate: roleSalaryInput => {
+                    if (isNaN(roleSalaryInput) || !roleSalaryInput) {
+                        console.log("\nPlease enter valid Role Salary!");
+                    }
+                    else {
+                        return true;
+                    }
+                },
+                filter: roleSalaryInput => {
+                    // clear the invalid input
+                    return !(isNaN(roleSalaryInput) || !roleSalaryInput) ? roleSalaryInput : ''
+                }
+            },
+            {
+                type: 'list',
+                message: 'What would you like to do?',
+                name: 'action',
+                choices: departmentChoices
+            }
+        ]).then((answers) => {
+            console.log(answers.roleTitle,answers.roleSalary,answers.action);
+            db.addRole(answers.roleTitle,Number(answers.roleSalary),Number(answers.action));
+        });
     })
-    }
-    else if(action === 'Add a Role'){
-        addRole().then(answers => {
-       console.log(answers);
-        })
+    };
+
+    showMenu().then(({ action }) => {
+        if (action === 'View all Departments') {
+            db.getDepartments();
+            showMenu();
         }
-});
+        else if (action === 'View all Roles') {
+            db.getRoles();
+            //showMenu();
+        }
+        else if (action === 'View all Employees') {
+            db.getEmployees();
+            //showMenu();
+        }
+        else if (action === 'Add a Department') {
+            addDepartment().then(answers => {
+                db.addDepartment(answers.deptName);
+            })
+            //showMenu();
+        }
+        else if (action === 'Add a Role') {
+            addRole();
+            //showMenu();
+        }
+    });
